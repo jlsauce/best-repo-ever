ff6856fb9399041da733fb64cce6081006d1e81d
jlsauce <Jerry.lai@sauceconsulting.com.au>
2022-09-16 10:46:05 +1000

    remove

--- app.js
+++ app.js
@@ -2,27 +2,4 @@ const app = {
     init: () => {
         console.log("app running");
     }
-    one : 1,
-    two : 2,
-    three: 3,
-
-    four: 4,
-    five: 5,
-    six: 6,
-
-    eleven : 11,
-    twelfth: 12,
-    thirdteen: 13,
-
-    test: 14,
-    test2: 15,
-    test3: 16
-
-    new commit
-    another commit
-    3rd try
-
-    another one
-    second one
-    third one
 };
